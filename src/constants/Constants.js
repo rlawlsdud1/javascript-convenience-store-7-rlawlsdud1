2b50a3a5ee1aa03138ca4c4c82d9bb65ae363ff0
KIMJINYOUNG <k5444862@naver.com>
2024-11-09 21:24:12 +0900

    chore: 에러 메시지, 특수문자 상수 파일 추가

---
+++ src/constants/Constants.js
@@ -0,0 +1,16 @@
+export const SHOPPING_ERROR_MESSAGES = {
+  INVALID_PRODUCT_NAME: "[ERROR] 존재하지 않는 상품입니다. 다시 입력해 주세요.",
+  INVALID_FORMAT:
+    "[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요",
+  INVALID_QUANTITY: "[ERROR] 유효하지 않은 수량입니다. 다시 입력해 주세요",
+  QUANTITY_EXCEEDED:
+    "[ERROR] 재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요.",
+  DUPLICATE_PRODUCT_NAME: "[ERROR] 중복된 상품명이 있습니다. 다시 입력해주세요",
+};
+
+export const SPECIAL_CHARACTER = {
+  COMMA: ",",
+  LEFT_BRACKET: "[",
+  RIGHT_BRACKET: "]",
+  DASH: "-",
+};
