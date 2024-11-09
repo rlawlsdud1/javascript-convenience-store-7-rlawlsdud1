d3802f5db0bd6318dd00b2b489b4ba0fbebd9f88
KIMJINYOUNG <k5444862@naver.com>
2024-11-09 21:39:44 +0900

    chore: 쇼핑 중 보여지는 메세지들 상수화

--- src/constants/Constants.js
+++ src/constants/Constants.js
@@ -14,3 +14,13 @@ export const SPECIAL_CHARACTER = {
   RIGHT_BRACKET: "]",
   DASH: "-",
 };
+
+export const SHOPPING_MESSAGES = {
+  WELCOME_MESSAGE:
+    "안녕하세요. W편의점입니다.\n현재 보유하고 있는 상품입니다.\n",
+  REQUEST_PRODUCT_AND_QUANTITY_MESSAGE:
+    "\n구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])\n",
+  REQUEST_MEMBERSHIP_APPLIED: "\n멤버십 할인을 받으시겠습니까? (Y/N)\n",
+  ADDITIONAL_PURCHASE:
+    "\n감사합니다. 구매하고 싶은 다른 상품이 있나요? (Y/N)\n",
+};
