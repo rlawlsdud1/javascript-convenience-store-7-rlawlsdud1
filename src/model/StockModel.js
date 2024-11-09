026d6b2cb7818527c6fa68f1b98c9019a5a551ff
KIMJINYOUNG <k5444862@naver.com>
2024-11-09 20:40:14 +0900

    feat: 주어진 파일로부터 상품 재고 데이터를 파싱하는 StockModel 클래스 생성

---
+++ src/model/StockModel.js
@@ -0,0 +1,54 @@
+import { readFileSync } from "fs";
+
+class StockModel {
+  #stockList;
+
+  constructor() {
+    this.#stockList = this.#readDataFile("./public/products.md");
+  }
+
+  #readDataFile(filePath) {
+    try {
+      const data = readFileSync(filePath, "utf8");
+      return this.#parseData(data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  #parseData(data) {
+    const lines = data.trim().split("\n");
+    const headers = lines[0].trim().split(",");
+
+    return lines.slice(1).map((line) => {
+      const values = line.trim().split(",");
+      return this.#createStockObj(headers, values);
+    });
+  }
+
+  #createStockObj(headers, values) {
+    const obj = {};
+
+    for (let i = 0; i < headers.length; i++) {
+      obj[headers[i]] = this.#parseValue(headers[i], values[i]);
+    }
+
+    return obj;
+  }
+
+  #parseValue(header, value) {
+    if (header === "price" || header === "quantity") {
+      return Number(value);
+    }
+    if (value === "null") {
+      return null;
+    }
+    return value;
+  }
+
+  get stockList() {
+    return this.#stockList;
+  }
+}
+
+export default StockModel;
