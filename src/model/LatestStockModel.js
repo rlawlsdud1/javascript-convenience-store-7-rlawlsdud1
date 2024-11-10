d99014cdbf3df580c83990572de3ff56df790bad
KIMJINYOUNG <k5444862@naver.com>
2024-11-10 18:25:08 +0900

    feat: LatestStockModel 클래스에 재고를 최신화하는 메서드 추가

--- src/model/LatestStockModel.js
+++ src/model/LatestStockModel.js
@@ -25,6 +25,52 @@ class LatestStockModel {
     });
   }
 
+  #processPromotionStock(productName, remainingQuantity) {
+    const promotionProducts = this.#latestStock.filter(
+      (item) => item.name === productName && item.promotion
+    );
+
+    promotionProducts.forEach((product) => {
+      if (product.quantity >= remainingQuantity) {
+        product.quantity -= remainingQuantity;
+        remainingQuantity = 0;
+      } else {
+        remainingQuantity -= product.quantity;
+        product.quantity = 0;
+      }
+    });
+  }
+
+  #processRegularStock(productName, remainingQuantity) {
+    const regularProducts = this.#latestStock.filter(
+      (item) => item.name === productName && item.promotion === null
+    );
+
+    for (const product of regularProducts) {
+      if (product.quantity >= remainingQuantity) {
+        product.quantity -= remainingQuantity;
+        break;
+      } else {
+        remainingQuantity -= product.quantity;
+        product.quantity = 0;
+      }
+    }
+  }
+
+  updateStockQuantities(products) {
+    products.forEach(([productName, _, quantity]) => {
+      let remainingQuantity = quantity;
+
+      // 프로모션이 재고부터 차감
+      this.#processPromotionStock(productName, remainingQuantity);
+
+      // 남은 수량이 있다면 일반 재고에서 차감
+      if (remainingQuantity > 0) {
+        this.#processRegularStock(productName, remainingQuantity);
+      }
+    });
+  }
+
   get latestStock() {
     return this.#latestStock;
   }
